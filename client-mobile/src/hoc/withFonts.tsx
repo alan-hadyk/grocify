b408031934ac14e96754aba172ff07304946187a
alan-hadyk <alan.hadyk@gmail.com>
2023-12-15 13:55:14 +0100

    Fix typo in withFonts

--- client-mobile/src/hoc/withFonts.tsx
+++ client-mobile/src/hoc/withFonts.tsx
@@ -3,8 +3,8 @@ import { useLoadFonts } from "@client/hooks/useLoadFonts"
 
 /**
  * Higher-order component that loads needed fonts
- * @param {React.FC<IAppProps>>} Component - The component to wrap with this HOC.
- * @returns {React.FC<IAppProps>>} - The component wrapped with this HOC.
+ * @param {React.FC<IAppProps>} Component - The component to wrap with this HOC.
+ * @returns {React.FC<IAppProps>} - The component wrapped with this HOC.
  */
 export const withFonts = (Component: React.FC<IAppProps>) => (props: IAppProps) => {
   const { fontError, fontsLoaded } = useLoadFonts()
